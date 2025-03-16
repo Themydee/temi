9462f47e05a9405d0e1326752f7869e25668de58
Akanbi Bello Temidayo <68824201+Themydee@users.noreply.github.com>
2025-03-17 00:42:09 +0100

    Update PortfolioItems.jsx

--- src/components/portfolio/PortfolioItems.jsx
+++ src/components/portfolio/PortfolioItems.jsx
@@ -6,15 +6,20 @@ const PortfolioItems = ({ item }) => {
         <img src={item.image} alt="Portfolio Works" className='portfolio-img' />
         <h3 className="portfolio-title">{item.title}</h3>
         <p className="portfolio-desc">{item.desc}</p>
-        <span>
-           <a href={item.url} className="portfolio-btn" target="_blank" rel="noreferrer">
+       <ul>
+         <li style="display:inline-block;">
+            <a href={item.url} className="portfolio-btn" target="_blank" rel="noreferrer">
                 View <i className="bx bx-right-arrow-alt portfolio-btn-icon"></i>
             </a>
-        
+         </li>
+
+         <li style="display:inline-block;">
             <a href={item.code} className="portfolio-btn" target="_blank" rel="noreferrer">
                 Code <i className="bx bx-right-arrow-alt portfolio-btn-icon"></i>
             </a>
-        </span>
+         </li>
+         
+       </ul>
       
     </div>
   )
