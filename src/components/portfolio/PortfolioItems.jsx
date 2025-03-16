90997e67492ef072ad3000e8bf215f136d841e18
Akanbi Bello Temidayo <68824201+Themydee@users.noreply.github.com>
2025-03-17 00:43:26 +0100

    Update PortfolioItems.jsx

--- src/components/portfolio/PortfolioItems.jsx
+++ src/components/portfolio/PortfolioItems.jsx
@@ -7,20 +7,18 @@ const PortfolioItems = ({ item }) => {
         <h3 className="portfolio-title">{item.title}</h3>
         <p className="portfolio-desc">{item.desc}</p>
        <ul>
-         <li style="display:inline-block;">
-            <a href={item.url} className="portfolio-btn" target="_blank" rel="noreferrer">
-                View <i className="bx bx-right-arrow-alt portfolio-btn-icon"></i>
-            </a>
-         </li>
+           <li style="display:inline-block;">
+              <a href={item.url} className="portfolio-btn" target="_blank" rel="noreferrer">
+                  View <i className="bx bx-right-arrow-alt portfolio-btn-icon"></i>
+              </a>
+           </li>
 
-         <li style="display:inline-block;">
-            <a href={item.code} className="portfolio-btn" target="_blank" rel="noreferrer">
-                Code <i className="bx bx-right-arrow-alt portfolio-btn-icon"></i>
-            </a>
-         </li>
-         
-       </ul>
-      
+           <li style="display:inline-block;">
+              <a href={item.code} className="portfolio-btn" target="_blank" rel="noreferrer">
+                  Code <i className="bx bx-right-arrow-alt portfolio-btn-icon"></i>
+              </a>
+           </li>
+       </ul> 
     </div>
   )
 }
